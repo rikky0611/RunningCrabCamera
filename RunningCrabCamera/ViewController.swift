c2b34417fd9fa03f74c1088116ff5a8dd5f2d8a2
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-09-16 12:41:24 +0900

    Initial Commit

---
+++ RunningCrabCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RunningCrabCamera
+//
+//  Created by 荒川陸 on 2016/09/16.
+//  Copyright © 2016年 Riku Arakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
