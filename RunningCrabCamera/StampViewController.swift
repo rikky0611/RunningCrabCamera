65a634ad0eafa3163b3e817e5a1c67648e6ace78
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-09-19 13:25:08 +0900

    Add stampView

--- RunningCrabCamera/StampViewController.swift
+++ RunningCrabCamera/StampViewController.swift
@@ -9,18 +9,26 @@
 import UIKit
 
 class StampViewController: UIViewController {
-    @IBOutlet var imageView: UIImageView!
     var takenImage: UIImage!
+    let screenWidth = UIScreen.mainScreen().bounds.width
+    var stampView: StampView!
+    
+    var cameraFrame: CGRect {
+        return CGRectMake(0.0, ViewManager.navigationBarHeight(self), screenWidth, screenWidth*1.0)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.image = takenImage
-        imageView.addSubview(UIImageView(image: UIImage(named: "logo")))
+        
+        stampView =  UINib(nibName: "StampView", bundle: nil).instantiateWithOwner(self, options: nil)[0] as! StampView
+        stampView.frame = cameraFrame
+        stampView.configure(takenImage)
+        view.addSubview(stampView)
     }
     
     @IBAction func didTapActionButton() {
-        UIGraphicsBeginImageContext(imageView.bounds.size)
-        imageView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        UIGraphicsBeginImageContext(stampView.bounds.size)
+        stampView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         self.presentViewController(ShareActivityController.create(image), animated: true, completion: nil)
