715d0154c023b15953c7eb19106831ab5f9930e5
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-09-22 22:11:04 +0900

    Create AlbumViewController

---
+++ RunningCrabCamera/AlbumViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumViewController.swift
+//  RunningCrabCamera
+//
+//  Created by 荒川陸 on 2016/09/22.
+//  Copyright © 2016年 Riku Arakawa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AlbumViewController: UIViewController {
+    
+}
