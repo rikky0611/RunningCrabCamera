e57fd4fb4f10d783d10d7b10d329d316075ac9e0
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-09-24 14:33:54 +0900

    Add PhotoCollectionView

--- RunningCrabCamera/AlbumViewController.swift
+++ RunningCrabCamera/AlbumViewController.swift
@@ -10,5 +10,34 @@ import Foundation
 import UIKit
 
 class AlbumViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let nib = UINib(nibName: "PhotoCell", bundle: nil)
+        self.collectionView.registerNib(nib, forCellWithReuseIdentifier: "PhotoCell")
+    }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+extension AlbumViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PhotoCell", forIndexPath: indexPath) as! PhotoCell
+        cell.configure()
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print("tapped")
+    }
+
 }
