7e8b684f3c15846f78d08cbb94a47fede8d5f361
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-09-26 09:16:56 +0900

    Validate timer in background

--- RunningCrabCamera/AppDelegate.swift
+++ RunningCrabCamera/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var backgroundTaskID : UIBackgroundTaskIdentifier = 0
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
@@ -23,6 +24,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        self.backgroundTaskID = application.beginBackgroundTaskWithExpirationHandler(){
+            [weak self] in
+            application.endBackgroundTask((self?.backgroundTaskID)!)
+            self?.backgroundTaskID = UIBackgroundTaskInvalid
+        }
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
@@ -36,6 +42,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        func applicationDidBecomeActive(application: UIApplication) {
+            application.endBackgroundTask(self.backgroundTaskID)
+        }
     }
 
     func applicationWillTerminate(application: UIApplication) {
